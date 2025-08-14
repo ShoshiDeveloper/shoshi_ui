0df0c3445db9af8b5fe6c618837412f13cd46d2c
P.Lobachev <pavel.lobachev@finbeat.tech>
2025-08-15 01:14:49 +0500

    upgrade: add radius

--- lib/src/consts/radii.dart
+++ lib/src/consts/radii.dart
@@ -2,6 +2,7 @@ class SRadii {
   static const s0 = 0.0;
   static const s04 = 4.0;
   static const s08 = 8.0;
+  static const s12 = 12.0;
   static const s16 = 16.0;
   static const max = 10000.0;
 }
