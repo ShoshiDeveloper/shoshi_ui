00e0757dd5157365a1dcad1e010300bf0d286ed8
P.Lobachev <pavel.lobachev@finbeat.tech>
2025-08-15 19:19:49 +0500

    feat: icons

--- lib/shoshi_ui.dart
+++ lib/shoshi_ui.dart
@@ -3,6 +3,7 @@
 library;
 
 export 'src/builders/interaction_builder.dart';
+export 'src/consts/icons.dart';
 export 'src/consts/interaction_builder_states.dart';
 export 'src/consts/radii.dart';
 export 'src/consts/spacings.dart';
@@ -17,6 +18,7 @@ export 'src/theme/theme.dart';
 export 'src/utils/color_utils.dart';
 export 'src/widgets/button/button.dart';
 export 'src/widgets/button/button_style.dart';
+export 'src/widgets/icon.dart';
 export 'src/widgets/input/input.dart';
 export 'src/widgets/skeleton.dart';
 export 'src/widgets/toggle/toggle.dart';
---
+++ lib/src/widgets/icon.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shoshi_ui/src/consts/icons.dart';
+
+abstract class SIconData {
+  String? get getSvg;
+}
+
+enum SIconsFilled implements SIconData {
+  dangerTriangle,
+  shieldWarning,
+  success;
+
+  @override
+  String? get getSvg => filledIcons[name];
+}
+
+enum SIconsOutlined implements SIconData {
+  upChevron,
+  downChevron,
+  leftChevron,
+  rightChevron,
+  exit,
+  check,
+  plus;
+
+  @override
+  String? get getSvg => filledIcons[name];
+}
+
+class SIcon extends StatelessWidget {
+  const SIcon.outlined({required SIconsOutlined this.icon, this.size = 24, this.color, super.key});
+  const SIcon.filled({required SIconsFilled this.icon, this.size = 24, this.color, super.key});
+
+  final SIconData? icon;
+  final double size;
+  final Color? color;
+
+  @override
+  Widget build(final BuildContext context) {
+    final svgString = icon?.getSvg;
+    if (svgString == null) return SizedBox.shrink();
+
+    return SizedBox(
+      height: size,
+      width: size,
+      child: SvgPicture.string(
+        svgString,
+        colorFilter: ColorFilter.mode(color ?? Colors.white, BlendMode.srcIn),
+        height: size,
+        width: size,
+      ),
+    );
+  }
+}
